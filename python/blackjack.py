4e081bb12d9944de8b00f1f308e2268d088cc724
sakeogh <sinead.keogh@ul.ie>
2016-11-14 13:44:34 +0000

    Blackjack random shuffle

---
+++ python/blackjack.py
@@ -0,0 +1,9 @@
+import random
+#                                        J   Q   K   A
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+#print(cards) # To see the list before being shuffled
+
+random.shuffle(cards)
+
+print(cards) # To see the list after being shuffled
