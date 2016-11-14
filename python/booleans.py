a4e0cdb8c3a779c189a0d43784b866ab40fdf274
sakeogh <sinead.keogh@ul.ie>
2016-11-14 09:44:39 +0000

    Boolean different data

--- python/booleans.py
+++ python/booleans.py
@@ -1,32 +1,23 @@
 #print(1 == 1) # Expected output: True
 #print(1 == 2) # Expected output: False
-
 #print(1 > 2) # Expected output: False
-
 #print(1 >= 1) # Expected output: True
-
 #print(2 == 2) # Expected output: True
-
 #print(2 != 2) # Expected output: False
 
 #age = 30
-
 #print(age > 10) # Expected outcome: True
-
 #print(10 < age) # Expected outcome: True
-
 #print(age > 10 + 20) # Expected outcome: False
-
 #print(age + 20 > 10) # Expected outcome: True
 
-print('a' > 'z') # Expected outcome: False
+#print('a' > 'z') # Expected outcome: False
+#print('z' > 'a') # Expected outcome: True
+#print('apples' > 'oranges') # Expected outcome: False
+#print('oranges' > 'apples') # Expected outcome: True
+#print('cat' > 'car') # Expected outcome: True
+#print('car' > 'cat') # Expected outcome: False
 
-print('z' > 'a') # Expected outcome: True
-
-print('apples' > 'oranges') # Expected outcome: False
-
-print('oranges' > 'apples') # Expected outcome: True
-
-print('cat' > 'car') # Expected outcome: True
-
-print('car' > 'cat') # Expected outcome: False
+#print 'a' > 2 # Outcome: True
+answer = int(raw_input('What is 1 + 1? '))
+print(answer == 2)
