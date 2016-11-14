af12879aff2bf1a31755d740a20de52cfef9da6d
sakeogh <sinead.keogh@ul.ie>
2016-11-14 09:39:49 +0000

    Booleans alphabet

--- python/booleans.py
+++ python/booleans.py
@@ -9,12 +9,24 @@
 
 #print(2 != 2) # Expected output: False
 
-age = 30
+#age = 30
 
-print(age > 10) # Expected outcome: True
+#print(age > 10) # Expected outcome: True
 
-print(10 < age) # Expected outcome: True
+#print(10 < age) # Expected outcome: True
 
-print(age > 10 + 20) # Expected outcome: False
+#print(age > 10 + 20) # Expected outcome: False
 
-print(age + 20 > 10) # Expected outcome: True
+#print(age + 20 > 10) # Expected outcome: True
+
+print('a' > 'z') # Expected outcome: False
+
+print('z' > 'a') # Expected outcome: True
+
+print('apples' > 'oranges') # Expected outcome: False
+
+print('oranges' > 'apples') # Expected outcome: True
+
+print('cat' > 'car') # Expected outcome: True
+
+print('car' > 'cat') # Expected outcome: False
