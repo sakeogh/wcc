644e398901d5d0b0d6249e433e07153dbee2ea28
sakeogh <sinead.keogh@ul.ie>
2016-11-14 13:46:07 +0000

    Blackjack pop

--- python/blackjack.py
+++ python/blackjack.py
@@ -7,3 +7,12 @@ cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 random.shuffle(cards)
 
 print(cards) # To see the list after being shuffled
+
+# Round 1
+player_card1 = cards.pop()
+computer_card1 = cards.pop()
+
+print('Player card: ' + str(player_card1))
+print('Computer card:  ' + str(computer_card1))
+
+print(cards) # To see the list after two cards have been popped off.
