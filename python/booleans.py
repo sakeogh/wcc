8109c394735df3106ace505010da1683e96e5445
sakeogh <sinead.keogh@ul.ie>
2016-11-14 09:26:58 +0000

    Booleans age exercise

--- python/booleans.py
+++ python/booleans.py
@@ -1,12 +1,20 @@
 #print(1 == 1) # Expected output: True
-print(1 == 2) # Expected output: False
+#print(1 == 2) # Expected output: False
 
-print(1 > 2) # Expected output: False
+#print(1 > 2) # Expected output: False
 
-print(2 > 1) # Expected output: True
+#print(1 >= 1) # Expected output: True
 
-print(1 >= 1) # Expected output: True
+#print(2 == 2) # Expected output: True
 
-print(2 == 2) # Expected output: True
+#print(2 != 2) # Expected output: False
 
-print(2 != 2) # Expected output: False
+age = 30
+
+print(age > 10) # Expected outcome: True
+
+print(10 < age) # Expected outcome: True
+
+print(age > 10 + 20) # Expected outcome: False
+
+print(age + 20 > 10) # Expected outcome: True
