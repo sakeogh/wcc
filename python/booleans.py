8984d61e7600dc434a318a0fe676bec9e3aa884d
sakeogh <sinead.keogh@ul.ie>
2016-11-14 09:21:17 +0000

    Booleans predictions

--- python/booleans.py
+++ python/booleans.py
@@ -1,1 +1,12 @@
-print(1 == 1) # Expected output: True
+#print(1 == 1) # Expected output: True
+print(1 == 2) # Expected output: False
+
+print(1 > 2) # Expected output: False
+
+print(2 > 1) # Expected output: True
+
+print(1 >= 1) # Expected output: True
+
+print(2 == 2) # Expected output: True
+
+print(2 != 2) # Expected output: False
