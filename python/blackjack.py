1faa98bb402cd9195338742b44957ecb77a63c0f
sakeogh <sinead.keogh@ul.ie>
2016-11-15 15:59:34 +0000

    Blackjack surrender to hints

--- python/blackjack.py
+++ python/blackjack.py
@@ -20,36 +20,36 @@ decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h
 
 # Round 2
 if decision == 's':
+    player_card2 = 0
     print('Your card: ' + str(player_card1));
     computer_card2 = cards.pop()
     print('Computer cards:  ' + str(computer_card1) + ', ' + str(computer_card2))
 elif decision == 'h':
     player_card2 = cards.pop()
     computer_card2 = cards.pop()
-
-    print('Player card: ' + str(player_card1) + ', ' + str(player_card2))
-    print('Computer card:  ' + str(computer_card1) + ', ' + str(computer_card2))
+    print('Player card: ' + (str(player_card1) + ', ' + str(player_card2)))
+    print('Computer card:  ' + (str(computer_card1) + ', ' + str(computer_card2)))
 
 # Round 2 results
-player_card_total2 = player_card1 + player_card2
-computer_card_total2 = computer_card1 + computer_card2
+player_card_total2 = (player_card1 + player_card2)
+computer_card_total2 = (computer_card1 + computer_card2)
 
 if (player_card_total2 > 21) and (computer_card_total2 <= 21):
-    print('Game over. Computer wins.');
+    print('Game over. Computer wins.')
 elif (player_card_total2 <= 21) and (computer_card_total2 > 21):
-    print('Game over. You win.');
+    print('Game over. You win.')
 elif (player_card_total2 == 21) and (computer_card_total2 == 21):
-    print("Game over. It's a draw!");
+    print("Game over. It's a draw!")
 else:
     decision2 = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
 
 # Round 3
 if (decision2 == 's') and (computer_card_total2 <= 16):
-    print('Your card: ' + str(player_card_total2));
-    computer_card3 = cards.pop();
-    print('Computer cards:  ' + str(computer_card1) + ', ' + str(computer_card2) + ', ' + str(computer_card3));
+    print('Your card: ' + str(player_card_total2))
+    computer_card3 = cards.pop()
+    print('Computer cards:  ' + str(computer_card1) + ', ' + str(computer_card2) + ', ' + str(computer_card3))
 elif (decision2 == 's') and (computer_card_total2 > 16):
-    print('Your card: ' + str(player_card_total2));
+    print('Your card: ' + str(player_card_total2))
     print('Computer cards:  ' + str(computer_card1) + ', ' + str(computer_card2))
 elif (decision2 == 'h') and (computer_card_total2 <= 16):
     player_card3 = cards.pop()
